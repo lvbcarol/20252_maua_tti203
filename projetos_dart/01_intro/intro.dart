27819dac8da12dff2bca72030e2e79f546d0552b
Carolina <carolinalansoni@gmail.com>
2025-08-08 12:47:11 -0300

    aula 2

--- projetos_dart/01_intro/intro.dart
+++ projetos_dart/01_intro/intro.dart
@@ -1,3 +1,122 @@
 void main() {
-  print('Hello, Dart!');
+
+  /*const nome = 'Carol'; //constante, não pode mudar o valor
+  const sobrenome = 'LVB'; //constante, não pode mudar o valor
+  const nomeCompleto = '$nome $sobrenome'; //concatenação de strings
+  print(nomeCompleto); // saída: Carol LVB
+
+  final name;
+  print (name); // saída: null
+  print(name.runtimeType); // saída: Null
+
+  outroNome = "JOAO";
+  var outroNome = null; //variável que pode ser alterada, mas não pode ser redeclarada
+  print(outroNome); // saída: null
+  //ver se tudo isso está certo. testar e ler a diferença entre final e const*/
+
+
+
+  /*a = 2;
+  a = "hey";
+  //aqui, a não mudou de tipo, mas sim o valor que ela armazena
+  //isso é possível porque o Dart é uma linguagem de tipagem dinâmica
+  //tipo estático é melhor para performance e projetos robustos em empresas
+  //treinar para saber dizer se é estático ou dinâmico só de olhar no código*/
+
+
+
+  //var é um operador de inferência de tipo, o Dart vai descobrir o tipo da variável
+
+  /*int b = 2;
+  ++b; // operador de incremento (pode usar também b++)
+  print(b); // saída: 3
+
+
+  int a = 10;
+  a ~ / = 5; // operador de divisão inteira
+  print(a); // saída: 2*/
+
+
+  /*String idadeTextual = '24';
+  int idade = int.parse(idadeTextual); //converte string para inteiro
+  print(idade);
+  String pesoTextual = '70.5';
+  double peso = double.parse(pesoTextual); //converte string para double
+  print(peso);
+  String alturaTextual = '1.75';
+  double altura = double.parse(alturaTextual); //converte string para double
+  print(altura);
+  print(altura.runtimeType); //verifica o tipo de dado
+  //treinar e testar com tipos diferentes para ver a saída
+  //nunca vai dar "num" na saída*/ 
+
+
+  /*String letra = 'A';
+  print (letra * 10);*/
+
+
+
+
+  /*String nome = 'Carol';
+  String sobrenome= 'LVB';
+  String endereco = ''' 
+  Rua das Flores, 123
+  Apto 45, Bloco B
+  '''; //três aspas servem para pular linha
+
+
+
+  //em dart só pode concatenar strings
+  int idade = 30;
+  print ('Me chamo $nome ');
+  print ('Tenho $idade anos');
+  print ("Ano que vem terei ${idade + 1} anos");
+  //reparar que se abrir aspas duplas, fecha com aspas duplas, unica com unica. 
+  //as duas funcionam e pode usar as duas
+  print('Me chamo' + ' ' + nome + ' ' + sobrenome);
+  print('Tenho' +  $idade.toString() + 'anos');*/
+
+
+  //em alguns blocos, usar o void de cima que foi escrito só uma vez
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*void main(){
+  int a =2;
+  double b = 3.5;
+  String c = 'Dart Programming';
+  bool d = true;
+  num e = 5;
+  num f = 4.5;
+  print(a.runtimeType);
+  print(b.runtimeType);
+  print(c.runtimeType);
+  print(d.runtimeType);
+  print(e.runtimeType);
+  print(f.runtimeType);
+
+  //vc também pode acessar outras propiedades
+  print(2.runtimeType);
+  print(true.runtimeType);
+}*/
+
+
+
+/*void main() {
+  print('Hello, Dart!');
+}*/
